e0384933e0b865a063c440e8a222a87269f73d58
Ngotho Jeff <getngothonow@gmail.com>
2022-09-11 15:55:45 +0300

    lsearch ++

--- algorithms/lsearch.c
+++ algorithms/lsearch.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
-void *lsearch(void *, void *, int, int, int (*cmpfn)(void *, void *));
+void *lsearch(void *, void *, int, int, 
+		int (*cmpfn)(void *, void *));
 
 int intcmp(void *, void *);
 
 int stringcmp(void *, void *);
 
-void *lsearch(void *search_key, void *db, int elem_size, int len, int (*cmpfn)(void *, void *)){
+void *lsearch(void *search_key, void *db, 
+		int elem_size, int len, 
+		int (*cmpfn)(void *, void *)){
 
 	for(int i=0; i<len; i++){
 		void *tracker = (char *)db+ i*elem_size;
 		if (cmpfn(tracker, search_key)==0){
 			return tracker;
 		}
-
 	}
 	return NULL;
 }
@@ -23,19 +25,15 @@ void *lsearch(void *search_key, void *db, int elem_size, int len, int (*cmpfn)(v
 int intcmp(void *pa, void *pb){
 	int a = *(int *)pa;
 	int b = *(int *)pb;
-
 	return a - b;
 }
+
 //to compare strings.
 int stringcmp(void *str1, void *str2){
-
 	char *s1 = *(char **)str1;
 	char *s2 = *(char **)str2;
-
 	return strcmp(s1, s2);
 }
-
-
 
 int main(int argc, char **argv){
 
